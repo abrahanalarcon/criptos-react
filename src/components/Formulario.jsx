92747be61e7eb0827b7aa155e14f4024743b033a
abrahanalarcon <moiseshazim@gmail.com>
2024-02-26 20:19:18 -0400

    ultimo comit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled"
+import useSelectMonedas from "../hooks/useSelectMonedas"
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+    margin-top: 30px;
+    &:hover {
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`
+
+const Formulario = () => {
+
+  const [ SelectMonedas ] = useSelectMonedas();
+
+  SelectMonedas()
+
+
+  return (
+    <form>
+           
+           <InputSubmit
+              type="submit" 
+              value="Cotizar"
+            />
+
+
+    </form>
+  )
+}
+
+export default Formulario
---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+
+
+const useSelectMonedas = () => {
+    
+    const SelectMonedas = () => {
+    
+    }
+     
+    
+
+    return [ SelectMonedas ]
+}
+
+export default useSelectMonedas
